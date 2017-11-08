6f578086a059ad32f2bd37757ef63e40bccfb2a0
Igor Kuznetsov <igk@igk.ru>
2017-11-08 20:08:43 +0300

    Implementation auth routes with handlers

--- main.go
+++ main.go
@@ -19,6 +19,21 @@ func handleMain(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(``))
 }
 
+// /auth. Initial page redirecting
+func handleGitHubAuth(w http.ResponseWriter, r *http.Request) {
+}
+
+// /callback. Called by github after authorization is granted
+func handleGitHubCallback(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(``))
+}
+
+func handleGitHubSuccess(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(``))
+}
+
 func init() {
 	dotenv.File(".env")
 	if hostEnv, ok := os.LookupEnv("HOST"); ok {
@@ -28,6 +43,9 @@ func init() {
 
 func main() {
 	http.HandleFunc("/", handleMain)
+	http.HandleFunc("/auth", handleGitHubAuth)
+	http.HandleFunc("/success", handleGitHubSuccess)
+	http.HandleFunc("/callback", handleGitHubCallback)
 	fmt.Printf("Started running on %s\n", host)
 	fmt.Println(http.ListenAndServe(host, nil))
 }
