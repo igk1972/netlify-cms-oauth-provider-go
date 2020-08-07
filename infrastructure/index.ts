61d7020ab745f57d8fdcdad1b64f7212af017369
zephyrz73 <66984950+zephyrz73@users.noreply.github.com>
2020-08-07 15:39:48 -0700

    add certificate

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -4,11 +4,15 @@ import * as awsx from "@pulumi/awsx";
 import * as pulumi from "@pulumi/pulumi"
 import { config } from "process";
 import * as random from "@pulumi/random";
+import * as aws from "@pulumi/aws";
 
 // Step 1: Read stack inputs for Github key and Github secret
 const pulumiConfig = new pulumi.Config();
-const githubKey = pulumiConfig.require("githubKey");
-const githubSecret = pulumiConfig.requireSecret("githubSecret")
+const cmsStackConfig = {
+    githubKey : pulumiConfig.require("githubKey"),
+    githubSecret : pulumiConfig.requireSecret("githubSecret"),
+    targetDomain : pulumiConfig.require("targetDomain")
+}
 
 // Step 1: Create an ECS Fargate cluster.
 const cluster = new awsx.ecs.Cluster("cluster");
@@ -67,11 +71,11 @@ const appService = new awsx.ecs.FargateService("app-svc", {
                 },
                 {
                     name: "GITHUB_KEY",
-                    value: githubKey
+                    value: cmsStackConfig.githubKey
                 },
                 {
                     name: "GITHUB_SECRET",
-                    value: githubSecret
+                    value: cmsStackConfig.githubSecret
                 }
             ]
         },
@@ -79,5 +83,93 @@ const appService = new awsx.ecs.FargateService("app-svc", {
     desiredCount: 1,
 });
 
+//-------------------Config Certification---///
+// Get a east provider
+const eastRegion = new aws.Provider("east", {
+    profile: aws.config.profile,
+    region: "us-east-1", // Per AWS, ACM certificate must be in the us-east-1 region.
+});
+
+//Creating a Certificate with the target domain
+const certificate = new aws.acm.Certificate("certificate", {
+    domainName: cmsStackConfig.targetDomain,
+    validationMethod: "DNS",
+}, { provider: eastRegion });
+
+const domainParts = getDomainAndSubdomain(cmsStackConfig.targetDomain);
+const hostedZoneId = aws.route53.getZone({ name: domainParts.parentDomain }, { async: true }).then(zone => zone.zoneId);
+
+const tenMinutes = 60 * 10;
+
+/**
+ *  Create a DNS record to prove that we _own_ the domain we're requesting a certificate for.
+ *  See https://docs.aws.amazon.com/acm/latest/userguide/gs-acm-validate-dns.html for more info.
+ */
+const certificateValidationDomain = new aws.route53.Record(`${cmsStackConfig.targetDomain}-validation`, {
+    name: certificate.domainValidationOptions[0].resourceRecordName,
+    zoneId: hostedZoneId,
+    type: certificate.domainValidationOptions[0].resourceRecordType,
+    records: [certificate.domainValidationOptions[0].resourceRecordValue],
+    ttl: tenMinutes,
+});
+
+// Split a domain name into its subdomain and parent domain names.
+// e.g. "www.example.com" => "www", "example.com".
+function getDomainAndSubdomain(domain: string): { subdomain: string, parentDomain: string } {
+    const parts = domain.split(".");
+    if (parts.length < 2) {
+        throw new Error(`No TLD found on ${domain}`);
+    }
+    // No subdomain, e.g. awesome-website.com.
+    if (parts.length === 2) {
+        return { subdomain: "", parentDomain: domain };
+    }
+
+    const subdomain = parts[0];
+    parts.shift();  // Drop first element.
+    return {
+        subdomain,
+        // Trailing "." to canonicalize domain.
+        parentDomain: parts.join(".") + ".",
+    };
+}
+
+/**
+ * This is a _special_ resource that waits for ACM to complete validation via the DNS record
+ * checking for a status of "ISSUED" on the certificate itself. No actual resources are
+ * created (or updated or deleted).
+ *
+ * See https://www.terraform.io/docs/providers/aws/r/acm_certificate_validation.html for slightly more detail
+ * and https://github.com/terraform-providers/terraform-provider-aws/blob/master/aws/resource_aws_acm_certificate_validation.go
+ * for the actual implementation.
+ */
+const certificateValidation = new aws.acm.CertificateValidation("certificateValidation", {
+    certificateArn: certificate.arn,
+    validationRecordFqdns: [certificateValidationDomain.fqdn],
+}, { provider: eastRegion });
+
+// Creates a new Route53 DNS record pointing the domain to the CloudFront distribution.
+function createAliasRecord(
+    targetDomain: string, lb: awsx.elasticloadbalancingv2.ApplicationLoadBalancer): aws.route53.Record {
+    const domainParts = getDomainAndSubdomain(targetDomain);
+    const hostedZoneId = aws.route53.getZone({ name: domainParts.parentDomain }, { async: true }).then(zone => zone.zoneId);
+    return new aws.route53.Record(
+        targetDomain,
+        {
+            name: domainParts.subdomain,
+            zoneId: hostedZoneId,
+            type: "A",
+            aliases: [
+                {
+                    name: lb.loadBalancer.dnsName,
+                    zoneId: lb.loadBalancer.zoneId,
+                    evaluateTargetHealth: true,
+                },
+            ],
+        });
+}
+
+const aRecord = createAliasRecord(cmsStackConfig.targetDomain, alb);
+
 // Step 5: Export the Internet address for the service.
-export const url = web.endpoint.hostname;
+export const rawEndpointUrl = web.endpoint.hostname;
